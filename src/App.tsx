0d58f6f8f4f49ef25a189b77fc66d64689950405
Lucas Rodrigues <lukinhasr1423@gmail.com>
2023-01-30 20:24:20 -0300

    criado os componentes formulario. botao e lista

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Formulario from './components/Formulario';
+import Lista from './components/Lista';
 
 function App() {
   return (
     <div className="App">
-      Olá
+      <Formulario />
+      <Lista />
     </div>
   );
 }

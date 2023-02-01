a54276643f178c8e4998ffc5ca782a41fe86ee1c
Lucas Rodrigues <lukinhasr1423@gmail.com>
2023-02-01 20:31:43 -0300

    lista refatorada

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Item from "./item";
 import style from './Lista.module.scss'
 
 export default function Lista() {
@@ -17,10 +18,10 @@ export default function Lista() {
       <h2>Estudos do dia</h2>
       <ul>
         {tarefas.map((item, index) => (
-          <li key={index} className={style.item}>
-            <h3>{item.tarefa}</h3>
-            <span>{item.tempo}</span>
-          </li>
+          <Item 
+            {...item}
+            key={index}
+          />
         ))}
       </ul>
     </aside>

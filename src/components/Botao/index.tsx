6b1f467036e79d61a8421ae819dbacfba10a33a1
Lucas Rodrigues <lukinhasr1423@gmail.com>
2023-02-02 09:53:55 -0300

    criado a lógica de adicionar tarefa

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import style from './Botao.module.scss'
 
-class Botao extends React.Component<{ children: string }> {
+class Botao extends React.Component<{ 
+  children: string, 
+  type?: "button" | "submit" | "reset" | undefined
+}> {
   render() {
+    const { type = "button" } = this.props
     return (
-      <button className={style.botao}>
+      <button className={style.botao} type={type}>
         {this.props.children}
       </button>
     )
--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,23 +1,11 @@
-import React, { useState } from "react";
+import { ITarefa } from "../../types/ITarefa";
 import Item from "./item";
 import style from './Lista.module.scss'
 
-export default function Lista() {
-  const [ tarefas, setTarefas ] = useState([{
-    tarefa: 'React',
-    tempo: '02:00:00'
-  }, {
-    tarefa: 'Javascript',
-    tempo: '01:00:00'
-  }, {
-    tarefa: 'Typescript',
-    tempo: '03:00:00'
-  }])
+export default function Lista({ tarefas }: { tarefas: ITarefa[] }) {
   return (
     <aside className={style.listaTarefas}>
-      <h2 onClick={() => {
-        setTarefas([...tarefas, { tarefa: "Estudar estados", tempo: "05:00:00" }])
-      }}>Estudos do dia</h2>
+      <h2>Estudos do dia</h2>
       <ul>
         {tarefas.map((item, index) => (
           <Item 

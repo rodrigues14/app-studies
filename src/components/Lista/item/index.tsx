f4c0c9d370a0da2bf57e4b037d0fe2fbe2ff5996
Lucas Rodrigues <lukinhasr1423@gmail.com>
2023-02-02 19:10:00 -0300

    tarefa atualizada, add id

--- src/components/Lista/item/index.tsx
+++ src/components/Lista/item/index.tsx
@@ -1,6 +1,9 @@
+import { ITarefa } from '../../../types/ITarefa'
 import style from '../Lista.module.scss'
 
-export default function Item({ tarefa, tempo }: { tarefa: string, tempo: string }) {
+export default function Item({ tarefa, tempo, selecionado, completado, id }: ITarefa) {
+  console.log("Item atual: ", { tarefa, tempo, selecionado, completado, id });
+  
   return (
     <li className={style.item}>
       <h3>{tarefa}</h3>
